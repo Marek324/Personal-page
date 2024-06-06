ca26a7cf8ceceb683de89d1543fa6af27a05ebdc
Marek324 <hrmajo1@gmail.com>
2024-06-06 19:04:06 +0200

    Mostly finished Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
-import LanguageSelector from "./LanguageSelector";
+import LanguageSelector from "./header/LanguageSelector";
+import Menu from "./header/Menu";
+import Logo from "./header/Logo";
 
 function Header() {
     return (
-        <header>
+        <header className="sticky ml-auto py-4 w-32 flex flex-col items-center justify-center h-screen select-none">
+            <Logo />
+            <Menu />
             <LanguageSelector />
         </header>
     );
---
+++ src/components/header/Menu.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from "react-i18next";
+
+function Menu() {
+    const { t } = useTranslation();
+    return (
+        <>
+            <nav className="w-full flex flex-col items-center justify-center font-black text-center text-text/65 my-auto">
+                <a
+                    href="#"
+                    className="w-full h-10 p-2 transition-colors duration-500  hover:text-text group"
+                >
+                    <p className="inline transition-all text-base duration-500 border-transparent border-b-2 group-hover:text-xl group-hover:border-text">
+                        {t("menu.about")}
+                    </p>
+                </a>
+                <a
+                    href="#"
+                    className="w-full h-10 p-2 transition-colors duration-500  hover:text-text group"
+                >
+                    <p className="inline transition-all text-base duration-500 border-transparent border-b-2 group-hover:text-xl group-hover:border-text">
+                        {t("menu.education")}
+                    </p>
+                </a>
+                <a
+                    href="#"
+                    className="w-full h-10 p-2 transition-colors duration-500  hover:text-text group"
+                >
+                    <p className="inline transition-all text-base duration-500 border-transparent border-b-2 group-hover:text-xl group-hover:border-text">
+                        {t("menu.skills")}
+                    </p>
+                </a>
+                <a
+                    href="#"
+                    className="w-full h-10 p-2 transition-colors duration-500  hover:text-text group"
+                >
+                    <p className="inline transition-all text-base duration-500 border-transparent border-b-2 group-hover:text-xl group-hover:border-text">
+                        {t("menu.contact")}
+                    </p>
+                </a>
+            </nav>
+        </>
+    );
+}
+
+export default Menu;
