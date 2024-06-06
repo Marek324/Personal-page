302847a95567d0348b0f7f4a000a813b3a88ca06
Marek324 <hrmajo1@gmail.com>
2024-06-06 19:04:06 +0200

    Mostly finished Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Suspense } from "react";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 function App() {
     return (
-        <Suspense fallback="Loading...">
-            <Header />
-        </Suspense>
+        <div className="text-text bg-background flex font-anonymous-pro">
+            <Suspense fallback="Loading...">
+                <Header />
+                <Body />
+            </Suspense>
+        </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                text: "#e2e6ee",
+                background: "#0a0e16",
+                primary: "#8da8d8",
+                secondary: "#1f4180",
+                accent: "#3873df",
+            },
+            fontFamily: {
+                "anonymous-pro": ['"Anonymous Pro"', "monospace"],
+            },
+        },
+    },
+    plugins: [],
+};
