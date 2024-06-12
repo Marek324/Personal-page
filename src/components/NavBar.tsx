3666e8b0e5b9a394264a3dcba02fcc64507963f8
Marek324 <hrmajo1@gmail.com>
2024-06-12 13:19:50 +0200

    Renamed Header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Logo from "./header/Logo";
 
 function Header() {
     return (
-        <header className="sticky ml-auto py-4 w-32 flex flex-col items-center justify-center h-screen select-none">
+        <header className="static py-4 w-1/6 flex flex-col items-center justify-center h-screen select-none">
             <Logo />
             <Menu />
             <LanguageSelector />
