071e5acf7f19e05a91c82aa918a1b5f03f44ebe5
Marek324 <hrmajo1@gmail.com>
2024-06-12 13:21:07 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
-import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 import Body from "./components/Body";
 
 function App() {
     return (
         <div className="text-text bg-background flex font-anonymous-pro">
             <Suspense fallback="Loading...">
-                <Header />
                 <Body />
+                <NavBar />
             </Suspense>
         </div>
     );
