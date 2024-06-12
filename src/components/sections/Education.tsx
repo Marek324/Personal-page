7282396026608b0c204356614beb86afb2d450f3
Marek324 <hrmajo1@gmail.com>
2024-06-12 15:11:18 +0200

    Working version of education section

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -1,12 +1,38 @@
 import { useTranslation } from "react-i18next";
 
+interface EducationItem {
+    school: string;
+    degree: string;
+    field: string;
+    year: string;
+}
+
 function Education() {
     const { t } = useTranslation();
 
+    const education: EducationItem[] = t("education.content", {
+        returnObjects: true,
+    });
+
     return (
         <section>
-            <h2>{t("education_section.title")}</h2>
-            <p>Some education details here...</p>
+            <h2 className="text-5xl font-bold text-center pb-8">
+                {t("education.title")}
+            </h2>
+            <ul className="py-8">
+                {education.map((item, index) => (
+                    <li key={index} className="flex">
+                        <p className="h-full flex-start w-1/6">{item.year}</p>
+                        <div className="flex flex-col ">
+                            <p className="font-bold text-accent">
+                                {item.school}
+                            </p>
+                            <p>{item.degree}</p>
+                            <p>{item.field}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 }
