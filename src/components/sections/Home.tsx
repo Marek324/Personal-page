c3e080be3ec6c2b54942b4c74abe56e2e906d008
Marek324 <hrmajo1@gmail.com>
2024-06-13 13:36:25 +0200

    Working version of home section

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,9 +1,24 @@
-// import meImg from "../../assets/tablo_c.jpg";
+import meImg from "../../assets/tablo_small_c.jpg";
+import { Trans } from "react-i18next";
 
 function Home() {
     return (
-        <section id="sec_home">
-            <h1>Home</h1>
+        <section id="sec_home" className="flex h-96">
+            <div className="flex-start basis-2/3 flex flex-col justify-center">
+                <p className="text-center text-4xl p-5 ">
+                    <Trans
+                        i18nKey="home"
+                        components={{
+                            1: <span className="text-accent font-black" />,
+                        }}
+                    />
+                </p>
+            </div>
+            <img
+                src={meImg}
+                alt="Image of me"
+                className="flex-end basis-1/3 object-contain"
+            />
         </section>
     );
 }
