7d5a4a4d65fe9e452790ccf4be45f60a4c81fe2c
Marek324 <hrmajo1@gmail.com>
2024-06-17 19:11:44 +0200

    Working version of Contacts section

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,4 +1,21 @@
 import { useTranslation } from "react-i18next";
+import ContactItem from "../Contact/ContactItem";
+
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+
+const contactItems = [
+    {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/hric-marek/",
+        icon: faLinkedin as IconProp,
+    },
+    {
+        name: "GitHub",
+        link: "https://github.com/Marek324",
+        icon: faSquareGithub as IconProp,
+    },
+];
 
 function Contact() {
     const { t } = useTranslation();
@@ -7,7 +24,16 @@ function Contact() {
             <h2 className="text-5xl font-bold text-center text-accent">
                 {t("contact")}
             </h2>
-            <p>Get in touch with us.</p>
+            <div className="flex justify-center items-center">
+                {contactItems.map((item, index) => (
+                    <ContactItem
+                        key={index}
+                        name={item.name}
+                        link={item.link}
+                        icon={item.icon}
+                    />
+                ))}
+            </div>
         </section>
     );
 }
