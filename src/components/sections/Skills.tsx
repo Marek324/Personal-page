6f09c0febaaac2d44ba7f618543be9588d6b951e
Marek324 <hrmajo1@gmail.com>
2024-06-17 18:39:24 +0200

    Working version of Skills section

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,12 +1,30 @@
+import { useTranslation } from "react-i18next";
+
+interface SkillsItem {
+    name: string;
+    level: string;
+}
+
 function Skills() {
+    const { t } = useTranslation();
+
+    const skills: SkillsItem[] = t("skills.content", {
+        returnObjects: true,
+    });
     return (
-        <section id="sec_skills">
-            <h2>Skills</h2>
-            <ul>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>GraphQL</li>
-                <li>Node.js</li>
+        <section id="sec_skills" className="py-8">
+            <h2 className="text-5xl font-bold text-center text-accent">
+                {t("skills.title")}
+            </h2>
+            <ul className="py-8">
+                {skills.map((item, index) => (
+                    <li key={index} className="flex mx-auto py-2">
+                        <p className="font-bold flex-start basis-1/4 text-accent">
+                            {item.name}
+                        </p>
+                        <p className="h-full  w-1/6">{item.level}</p>
+                    </li>
+                ))}
             </ul>
         </section>
     );
