96a0e7fc2724b05d39dcb11745356ee6efc63e51
Marek324 <hrmajo1@gmail.com>
2024-06-17 19:11:44 +0200

    Working version of Contacts section

--- src/components/Contact/ContactItem.tsx
+++ src/components/Contact/ContactItem.tsx
@@ -1,3 +1,31 @@
-function ContactItem() {}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface ContactItemProps {
+    name: string;
+    link: string;
+    icon: IconProp;
+}
+
+function ContactItem(props: ContactItemProps) {
+    const { name, link, icon } = props;
+
+    return (
+        <div className="flex flex-col py-6 px-8">
+            <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className="text-center group"
+            >
+                <FontAwesomeIcon
+                    icon={icon}
+                    className="text-8xl text-primary group-hover:text-accent transition-colors duration-300"
+                />
+                <h2 className="">{name}</h2>
+            </a>
+        </div>
+    );
+}
 
 export default ContactItem;
