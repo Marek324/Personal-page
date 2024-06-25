0aed15bef6640543cd06bfb29198ad7219e0707b
Marek324 <hrmajo1@gmail.com>
2024-06-25 14:00:31 +0200

    Updated Footer

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -2,9 +2,13 @@ import { useTranslation } from "react-i18next";
 
 function Footer() {
     const { t } = useTranslation();
+
     return (
         <footer>
-            <p className="text-center">{t("footer")}</p>
+            <p className="text-center">
+                &copy; 2024 Marek Hric. {t("footer.line1")}
+            </p>
+            <p className="text-center">{t("footer.line2")}</p>
         </footer>
     );
 }
