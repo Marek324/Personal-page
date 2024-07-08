93fbce103808953c526622448b452cd3a367207c
Marek324 <hrmajo1@gmail.com>
2024-07-08 22:50:41 +0200

    Centered About me section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -4,12 +4,14 @@ function About() {
     const { t } = useTranslation();
 
     return (
-        <section id="sec_about" className="">
-            <h2 className="text-5xl font-bold text-center pb-8 text-accent">
+        <section id="sec_about" className="grid grid-cols-9">
+            <h2 className="text-5xl font-bold text-center pb-8 text-accent col-span-9">
                 {t("about.title")}
             </h2>
 
-            <p className="text-center">{t("about.content")}</p>
+            <p className="text-center col-span-7 col-start-2">
+                {t("about.content")}
+            </p>
         </section>
     );
 }
