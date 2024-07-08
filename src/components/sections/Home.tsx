571a19b6b4cfcc0a1de6d9290d2bb88ae1a31404
Marek324 <hrmajo1@gmail.com>
2024-07-08 22:48:27 +0200

    Centered Home section

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -4,18 +4,20 @@ import { useTranslation, Trans } from "react-i18next";
 function Home() {
     const { t } = useTranslation();
     return (
-        <section id="sec_home" className="grid grid-cols-4 gap-5">
-            <div className="col-span-3 grid grid-rows-5">
-                <p className="text-center text-4xl row-start-3">
-                    <Trans
-                        i18nKey={t("home")}
-                        components={{
-                            1: <span className="text-accent font-black" />,
-                        }}
-                    />
-                </p>
+        <section id="sec_home" className="grid grid-cols-9 gap-5">
+            <div className="col-span-7 col-start-2 grid grid-cols-5 gap-5">
+                <div className="col-span-2 col-start-2 grid grid-rows-7">
+                    <p className="text-center text-4xl row-span-5 row-start-2">
+                        <Trans
+                            i18nKey={t("home")}
+                            components={{
+                                1: <span className="text-accent font-black" />,
+                            }}
+                        />
+                    </p>
+                </div>
+                <img src={meImg} alt="Image of me" className="object-contain" />
             </div>
-            <img src={meImg} alt="Image of me" className="object-contain" />
         </section>
     );
 }
